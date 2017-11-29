75f29b0c8d6b1bfab6542dbe481e5c28e7697118
hoangdinhtuan07 <hoangdinhtuan07@gmail.com>
2017-11-29 16:57:01 +0700

    read

---
+++ src/main/java/com/itss/Controller/BookDeleteController.java
@@ -0,0 +1,30 @@
+package com.itss.Controller;
+
+import com.itss.basic.BasicController;
+
+import java.util.Vector;
+
+/**
+ * Created by HDT on 11/29/2017.
+ */
+public class BookDeleteController implements BasicController {
+    @Override
+    public Vector<Object> getModel() {
+        return null;
+    }
+
+    @Override
+    public boolean validateObject() {
+        return false;
+    }
+
+    @Override
+    public void updateData() {
+
+    }
+
+    @Override
+    public void selectData() {
+
+    }
+}
