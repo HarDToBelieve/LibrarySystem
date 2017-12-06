4c55a8f577ccd2ca81743f7b23f957d33c63ac90
hardtobelieve <tuanit96@gmail.com>
2017-12-06 22:40:20 +0700

    Merge branch 'vu.ba.05' of https://github.com/HarDToBelieve/LibrarySystem
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/itss/Boundary/MainWindow.java
+++ src/main/java/com/itss/Boundary/MainWindow.java
@@ -3,8 +3,11 @@ package com.itss.Boundary;
 import com.itss.Controller.ObserverController;
 import com.itss.basic.BasicView;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
 
 /**
@@ -20,52 +23,53 @@ public class MainWindow extends JFrame {
         this.setPreferredSize(new Dimension(1000, 600));
         Vector<BasicView> tmp = new Vector<>();
 
+        try {
+            Image img = ImageIO.read(new File(System.getProperty("user.dir") + "\\resource\\Webp.net-resizeimage (2).png"));
+            Icon icon = new ImageIcon(img);
+            menuTabbed.addTab("", icon, new JPanel());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         if ( job.equals("librarian") ) {
             BookRegistrationForm brf = new BookRegistrationForm();
-            menuTabbed.add(brf.getMainPanel(), "Add new book");
+            menuTabbed.add(brf.getMainPanel(), "<html><body><table height='1000'>Add new book</table></body></html>");
             tmp.add(brf);
 
             BookCopyRegistrationForm bcrf = new BookCopyRegistrationForm("");
-            menuTabbed.add(bcrf.getMainPanel(), "Add new copy");
+            menuTabbed.add(bcrf.getMainPanel(), "<html><body><table height='1000'>Add new copy</table></body></html>");
             tmp.add(bcrf);
 
             BookDeleteForm bdv = new BookDeleteForm();
-            menuTabbed.add(bdv.getMainPanel(), "Delete book");
+            menuTabbed.add(bdv.getMainPanel(), "<html><body><table height='1000'>Delete book</table></body></html>");
             tmp.add(bdv);
 
             ReturnBookForm rbv = new ReturnBookForm();
-            menuTabbed.add(rbv.getMainPanel(), "Return Book");
+            menuTabbed.add(rbv.getMainPanel(), "<html><body><table height='1000'>Return Book</table></body></html>");
             tmp.add(rbv);
 
             IssueCardForm icv = new IssueCardForm();
-            menuTabbed.add(icv.getMainPanel(), "Issue Card");
+            menuTabbed.add(icv.getMainPanel(), "<html><body><table height='1000'>Issue Card</table></body></html>");
             tmp.add(icv);
         }
         else if ( job.equals("student") ) {
             BorrowBookForm bbv = new BorrowBookForm(username, cardno);
-            menuTabbed.add(bbv.getMainPanel(), "Borrow Book");
+            menuTabbed.add(bbv.getMainPanel(), "<html><body><table height='1000'>Borrow Book</table></body></html>");
             tmp.add(bbv);
         }
 
         ObserverController.setViews(tmp);
         UserLogoutForm ulf = new UserLogoutForm();
-        menuTabbed.add(ulf.getMainPanel(), "Logout");
+        menuTabbed.add(ulf.getMainPanel(), "<html><body><table height='1000'>Logout</table></body></html>");
 
+        menuTabbed.setSelectedIndex(1);
 
         menuTabbed.setTabPlacement(JTabbedPane.LEFT);
 
-//        menuTabbed.addChangeListener(e -> {
-//            if ( menuTabbed.getSelectedIndex() == 0 ) {
-//                menuTabbed.setComponentAt(0, new BookRegistrationForm().getMainPanel());
-//            }
-//            else if ( menuTabbed.getSelectedIndex() == 1 )
-//                menuTabbed.setComponentAt(1, new BookCopyRegistrationForm().getMainPanel());
-//            else if ( menuTabbed.getSelectedIndex() == 2 )
-//                menuTabbed.setComponentAt(2, new BookDeleteForm().getMainPanel());
-//            else if ( menuTabbed.getSelectedIndex() == 3 )
-//                menuTabbed.setComponentAt(3, new ReturnBookForm().getMainPanel());
-//            else if ( menuTabbed.getSelectedIndex() == 4 )
-//                menuTabbed.setComponentAt(4, new IssueCardForm().getMainPanel());
-//        });
+        menuTabbed.addChangeListener(e -> {
+            if ( menuTabbed.getSelectedIndex() == 0 ) {
+                menuTabbed.setSelectedIndex(1);
+            }
+        });
     }
 }
